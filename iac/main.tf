0d32cce3d2670aafc90a971750b35d0acb09c1ab
codermarcos <coder.marcos@gmail.com>
2024-04-15 16:09:23 -0300

    🔥 remove terragrunt and upgrade dependencies On branch main Changes to be committed: 	modified:   .github/workflows/on_push_main.yaml 	modified:   iac/.terraform.lock.hcl 	new file:   iac/backend.tf 	modified:   iac/main.tf 	deleted:    iac/terragrunt.hcl

--- iac/main.tf
+++ iac/main.tf
@@ -1,21 +1,19 @@
-variable "project" {
-  description = "Name of the project"
-  type        = string
+variable "package_path" {
+	description = "Path to your packege json"
+	type        = string
+	default     = "../package.json"
 }
 
 variable "root_domain" {
   description = "Site domain"
   type        = string
+	default     = "codermarcos.zone"
 }
 
 variable "sub_domain" {
   description = "Site sub domain"
   type        = string
-}
-
-variable "dist_dir" {
-  description = "Dist folder to deploy"
-  type        = string
+	default     = "mr.codermarcos.zone"
 }
 
 locals {
@@ -30,8 +28,13 @@ locals {
     jpg  = "image/jpeg",
     gif  = "image/gif",
     png  = "image/png",
-    css  = "text/css"
+    css  = "text/css",
   }
+
+  package = jsondecode(file(var.package_path))
+
+	name     = local.package.name
+	dist_dir = pathexpand(join("", [".", local.package.bundle]))
 }
 
 data "aws_acm_certificate" "issued" {
@@ -43,9 +46,15 @@ data "aws_cloudfront_cache_policy" "cache_default" {
   name = "Managed-CachingOptimized"
 }
 
+# TODO: Use it when move it to a shared repository
+# data "aws_cloudfront_function" "redirects" {
+# 	name = "redirects-to-index"
+# 	stage = "LIVE"
+# }
+
 resource "aws_cloudfront_function" "redirects" {
   name    = "redirects-to-index"
-  runtime = "cloudfront-js-1.0"
+  runtime = "cloudfront-js-2.0"
   comment = "Redirect to index when omit index html"
   publish = true
   code    = file("${path.module}/functions.js")
@@ -54,7 +63,8 @@ resource "aws_cloudfront_function" "redirects" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.frontend_bucket.bucket_domain_name
-    origin_id   = var.project
+
+    origin_id   = local.name
   }
 
   aliases = [
@@ -73,7 +83,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = var.project
+    target_origin_id = local.name
     compress         = true
 
     default_ttl = 86400 * 15
@@ -105,27 +115,48 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 }
 
 resource "aws_s3_bucket" "frontend_bucket" {
-  acl           = "public-read"
   force_destroy = true
-  bucket        = var.project
-
-  policy = <<EOF
-{
-  "Id": "bucket_policy_site",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "bucket_policy_site_main",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::${var.project}/*",
-      "Principal": "*"
-    }
-  ]
+  bucket        = local.name
 }
-EOF
+
+resource "aws_s3_bucket_acl" "frontend_bucket_acl" {
+	bucket = local.name
+	acl    = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.frontend_bucket_controls]
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+	bucket = local.name
+	policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = ["s3:GetObject"]
+        Effect    = "Allow"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
+        Principal = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_ownership_controls" "frontend_bucket_controls" {
+	bucket = local.name
+
+	rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+
+  depends_on = [aws_s3_bucket_public_access_block.frontend_bucket_access_block]
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_access_block" {
+	bucket = local.name
+
+	ignore_public_acls      = false
+	block_public_acls       = false
+	block_public_policy     = false
+	restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
@@ -141,18 +172,18 @@ resource "aws_s3_bucket_website_configuration" "website" {
 }
 
 resource "aws_s3_object" "frontend_object" {
-  for_each = fileset(var.dist_dir, "**")
+  for_each = fileset(local.dist_dir, "**")
   key      = each.value
-  source   = "${var.dist_dir}/${each.value}"
+  source   = "${local.dist_dir}/${each.value}"
   bucket   = aws_s3_bucket.frontend_bucket.bucket
 
   force_destroy = true
-  etag          = filemd5("${var.dist_dir}/${each.value}")
+  etag          = filemd5("${local.dist_dir}/${each.value}")
   content_type  = local.mime_type_mappings[concat(regexall("\\.([^\\.]*)$", each.value), [[""]])[0][0]]
 }
 
 output "static_files_folder" {
-  value = var.dist_dir
+  value = local.dist_dir
 }
 
 output "cloudfront_endpoint" {
