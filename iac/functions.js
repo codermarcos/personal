c814793aa52d7f513a3dd128bff03d8e04140112
codermarcos <coder.marcos@gmail.com>
2025-10-21 14:01:02 -0300

    🐛 fix bug related with index On branch main Changes to be committed: 	modified:   iac/functions.js 	modified:   iac/main.tf

--- iac/functions.js
+++ iac/functions.js
@@ -1,11 +1,7 @@
 function handler(event) {
 	var request = event.request;
 	if (request.uri !== "/" && (request.uri.endsWith("/") || request.uri.endsWith("/en"))) {
-		if (request.uri.endsWith("/")) {
-			request.uri = request.uri.concat("index.html");
-		} else {
-			request.uri = request.uri.concat("/index.html");
-		}
+		request.uri = request.uri.concat(request.uri.endsWith("/") ? "index.html" : "/index.html");
 	}
 	return request;
 }
--- iac/main.tf
+++ iac/main.tf
@@ -1,49 +1,49 @@
 variable "package_path" {
-	description = "Path to your packege json"
-	type        = string
-	default     = "../package.json"
+  description = "Path to your packege json"
+  type        = string
+  default     = "../package.json"
 }
 
 variable "root_domain" {
-	description = "Site domain"
-	type        = string
-	default     = "codermarcos.zone"
+  description = "Site domain"
+  type        = string
+  default     = "codermarcos.zone"
 }
 
 variable "sub_domain" {
-	description = "Site sub domain"
-	type        = string
-	default     = "mr.codermarcos.zone"
+  description = "Site sub domain"
+  type        = string
+  default     = "mr.codermarcos.zone"
 }
 
 locals {
-	mime_type_mappings = {
-		json = "application/json",
-		js   = "text/javascript",
-		xml  = "application/xml",
-		ico  = "image/x-icon",
-		txt  = "text/plain",
-		html = "text/html",
-		webp = "image/webp",
-		jpg  = "image/jpeg",
-		gif  = "image/gif",
-		png  = "image/png",
-		css  = "text/css",
-	}
+  mime_type_mappings = {
+    json = "application/json",
+    js   = "text/javascript",
+    xml  = "application/xml",
+    ico  = "image/x-icon",
+    txt  = "text/plain",
+    html = "text/html",
+    webp = "image/webp",
+    jpg  = "image/jpeg",
+    gif  = "image/gif",
+    png  = "image/png",
+    css  = "text/css",
+  }
 
-	package = jsondecode(file(var.package_path))
+  package = jsondecode(file(var.package_path))
 
-	name     = local.package.name
-	dist_dir = pathexpand(join("", [".", local.package.bundle]))
+  name     = local.package.name
+  dist_dir = pathexpand(join("", [".", local.package.bundle]))
 }
 
 data "aws_acm_certificate" "issued" {
-	domain   = "codermarcos.zone"
-	statuses = ["ISSUED"]
+  domain   = "codermarcos.zone"
+  statuses = ["ISSUED"]
 }
 
 data "aws_cloudfront_cache_policy" "cache_default" {
-	name = "Managed-CachingOptimized"
+  name = "Managed-CachingOptimized"
 }
 
 # TODO: Use it when move it to a shared repository
@@ -52,150 +52,149 @@ data "aws_cloudfront_cache_policy" "cache_default" {
 # 	stage = "LIVE"
 # }
 
-#resource "aws_cloudfront_function" "redirects" {
-#	name    = "redirects-to-index"
-#	runtime = "cloudfront-js-2.0"
-#	comment = "Redirect to index when omit index html"
-#	publish = true
-#	code    = file("${path.module}/functions.js")
-#}
+resource "aws_cloudfront_function" "redirects" {
+  name    = "redirects-to-index"
+  runtime = "cloudfront-js-2.0"
+  comment = "Redirect to index when omit index html"
+  publish = true
+  code    = file("${path.module}/functions.js")
+}
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
-	origin {
-		domain_name = aws_s3_bucket.frontend_bucket.bucket_domain_name
-		origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
-		origin_id   = aws_s3_bucket.frontend_bucket.bucket
-	}
+  origin {
+    domain_name              = aws_s3_bucket.frontend_bucket.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = aws_s3_bucket.frontend_bucket.bucket
+  }
 
-	aliases = [
-		var.root_domain,
-		var.sub_domain,
-	]
+  aliases = [
+    var.root_domain,
+    var.sub_domain,
+  ]
 
-	enabled             = true
-	is_ipv6_enabled     = true
-	http_version        = "http2and3"
-	comment             = "Frontend app"
-	default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  http_version        = "http2and3"
+  comment             = "${local.name} frontend app"
+  default_root_object = "index.html"
 
-	default_cache_behavior {
-		cache_policy_id = data.aws_cloudfront_cache_policy.cache_default.id
+  default_cache_behavior {
+    cache_policy_id = data.aws_cloudfront_cache_policy.cache_default.id
 
-		allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-		cached_methods   = ["GET", "HEAD", "OPTIONS"]
-		target_origin_id = aws_s3_bucket.frontend_bucket.bucket
-		compress         = true
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.frontend_bucket.bucket
+    compress         = true
 
-		default_ttl = 86400 * 15
-		max_ttl     = 31536000
-		min_ttl     = 1
+    default_ttl = 86400 * 15
+    max_ttl     = 31536000
+    min_ttl     = 1
 
-		viewer_protocol_policy = "redirect-to-https"
+    viewer_protocol_policy = "redirect-to-https"
 
-#		function_association {
-#			event_type = "viewer-request"
-#			function_arn = aws_cloudfront_function.redirects.arn
-#		}
-	}
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.redirects.arn
+    }
+  }
 
-	custom_error_response {
-		error_code         = 404
-		response_code      = 200
-		response_page_path = "/index.html"
-	}
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "./index.html"
+  }
 
   custom_error_response {
     error_code         = 403
     response_code      = 200
-    response_page_path = "/index.html"
+    response_page_path = "./index.html"
   }
 
-	restrictions {
-		geo_restriction {
-			restriction_type = "none"
-			locations        = []
-		}
-	}
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
-	viewer_certificate {
-		acm_certificate_arn            = data.aws_acm_certificate.issued.arn
-		minimum_protocol_version       = "TLSv1.2_2021"
-		ssl_support_method             = "sni-only"
-		cloudfront_default_certificate = false
-	}
+  viewer_certificate {
+    acm_certificate_arn            = data.aws_acm_certificate.issued.arn
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
+    cloudfront_default_certificate = false
+  }
 }
 
 resource "aws_s3_bucket" "frontend_bucket" {
-	force_destroy = true
-	bucket_prefix = local.name
+  force_destroy = true
+  bucket_prefix = local.name
 }
 
 resource "aws_s3_bucket_acl" "frontend_bucket_acl" {
-	bucket = aws_s3_bucket.frontend_bucket.bucket
-	acl    = "private"
-	depends_on = [aws_s3_bucket_ownership_controls.frontend_bucket_controls]
+  bucket     = aws_s3_bucket.frontend_bucket.bucket
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.frontend_bucket_controls]
 }
 
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
-	bucket = aws_s3_bucket.frontend_bucket.bucket
+  bucket = aws_s3_bucket.frontend_bucket.bucket
   policy = data.aws_iam_policy_document.cloudfront_oac_access.json
 }
 
 resource "aws_cloudfront_origin_access_control" "oac" {
-	name                              = "s3-cloudfront-oac"
-	description                       = "Grant cloudfront access to s3 bucket ${aws_s3_bucket.frontend_bucket.id}"
-	origin_access_control_origin_type = "s3"
-	signing_behavior                  = "always"
-	signing_protocol                  = "sigv4"
+  name                              = "${local.name}-s3-cloudfront-oac"
+  description                       = "Grant cloudfront access to s3 bucket ${aws_s3_bucket.frontend_bucket.id}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 data "aws_iam_policy_document" "cloudfront_oac_access" {
-	statement {
-		principals {
-			type        = "Service"
-			identifiers = ["cloudfront.amazonaws.com"]
-		}
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
 
-		actions = [
-			"s3:GetObject"
-		]
+    actions = [
+      "s3:GetObject"
+    ]
 
-		resources = [
-			aws_s3_bucket.frontend_bucket.arn,
-			"${aws_s3_bucket.frontend_bucket.arn}/*"
-		]
+    resources = [
+      aws_s3_bucket.frontend_bucket.arn,
+      "${aws_s3_bucket.frontend_bucket.arn}/*"
+    ]
 
-		condition {
-			test     = "StringEquals"
-			variable = "AWS:SourceArn"
-			values   = [aws_cloudfront_distribution.s3_distribution.arn]
-		}
-	}
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "frontend_bucket_controls" {
-	bucket = aws_s3_bucket.frontend_bucket.bucket
+  bucket = aws_s3_bucket.frontend_bucket.bucket
 
-	rule {
-		object_ownership = "BucketOwnerPreferred"
-	}
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_object" "frontend_object" {
-	for_each = fileset(local.dist_dir, "**")
-	key      = each.value
-	source   = "${local.dist_dir}/${each.value}"
-	bucket   = aws_s3_bucket.frontend_bucket.bucket
+  for_each = fileset(local.dist_dir, "**")
+  key      = each.value
+  source   = "${local.dist_dir}/${each.value}"
+  bucket   = aws_s3_bucket.frontend_bucket.bucket
 
-	force_destroy = true
-	etag          = filemd5("${local.dist_dir}/${each.value}")
-	content_type  = local.mime_type_mappings[concat(regexall("\\.([^\\.]*)$", each.value), [[""]])[0][0]]
+  force_destroy = true
+  etag          = filemd5("${local.dist_dir}/${each.value}")
+  content_type  = local.mime_type_mappings[concat(regexall("\\.([^\\.]*)$", each.value), [[""]])[0][0]]
 }
 
 output "static_files_folder" {
-	value = local.dist_dir
+  value = local.dist_dir
 }
 
 output "cloudfront_endpoint" {
-	value = aws_cloudfront_distribution.s3_distribution.domain_name
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
